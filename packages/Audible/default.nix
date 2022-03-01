256064e0e592db1a02cbca8e48c923219383cbaa
Benton Edmondson <bentonedmondson@gmail.com>
2022-03-01 15:30:45 -0500

    updated Audible to 0.7.0--working

--- packages/Audible/default.nix
+++ packages/Audible/default.nix
@@ -1,21 +1,23 @@
 { nixpkgs, ... }: nixpkgs.python3Packages.buildPythonPackage rec {
     pname = "Audible";
-    version = "0.6.0";
+    version = "0.7.0";
 
     src = nixpkgs.fetchFromGitHub {
         owner = "mkb79";
         repo = pname;
         rev = "v${version}";
-        sha256 = "1hqdrd2kk01qx3yna6hw5pzxgr19c3gwj2w4vxisgnh57m6pxnwm";
+        sha256 = "0wj8m6y3dvyd1gl3qgk811alsm6474yk9xnbq2rmrd5pa1infbvk";
     };
 
     propagatedBuildInputs = [
         nixpkgs.python3Packages.setuptools
         nixpkgs.python3Packages.beautifulsoup4
-        nixpkgs.python3Packages.httpx
+        nixpkgs.python39Packages.httpx
         nixpkgs.python3Packages.pbkdf2
         nixpkgs.python3Packages.pillow
         nixpkgs.python3Packages.pyaes
         nixpkgs.python3Packages.rsa
     ];
+
+    patches = [ ./unrestrict-httpx-dependency.patch ];
 }
